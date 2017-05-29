e2999f0cf43080c4af7efc94f143ee067e6a74ee
cloud4dev <cloud4dev@gmail.com>
2017-05-30 00:13:29 +0200

    Creación de proyecto

---
+++ src/main/java/cloud4dev/springboot/security/SpringBoot4securityBasicAuthApplication.java
@@ -0,0 +1,12 @@
+package cloud4dev.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4securityBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot4securityBasicAuthApplication.class, args);
+	}
+}
---
+++ src/test/java/cloud4dev/springboot/security/SpringBoot4securityBasicAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package cloud4dev.springboot.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot4securityBasicAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
